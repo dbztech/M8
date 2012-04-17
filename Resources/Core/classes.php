73dc5ff15c6aacf4c1481dcf0a123d947d047f8d
Brendan Boyle <brendancboyle@me.com>
2012-04-17 12:15:53 -0500

    Added basic database class

--- Resources/Core/classes.php
+++ Resources/Core/classes.php
@@ -4,5 +4,25 @@ foreach (glob("Classes/*.php") as $filename)
 {
     include $filename;
 }
+
+#Basic database interaction class
+class database
+{
+    // property declaration
+    private $dbuser = 'm8';
+    private $dbpassword = 'hunter3';
+    private $dbhost = 'localhost';
+    private $database = 'm8db';
+    private $prefix = 'meight';
+
+    // method declaration
+    public function info() {
+    	echo "Database: ".$this->database."<br />";
+        echo "Database User: ".$this->dbuser."<br />";
+        echo "Database Host: ".$this->dbhost."<br />";
+        echo "Database Prefix: ".$this->prefix."<br />";
+    }
+}
+
 #echo "Classes Initialized <br />";
 ?>
