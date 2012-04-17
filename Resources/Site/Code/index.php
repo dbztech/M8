77ef99e362edd2ba819f6bfe1c4293109998be7a
Brendan Boyle <brendancboyle@me.com>
2012-04-17 18:00:10 -0500

    Added splash and fonts

--- Resources/Site/Code/index.php
+++ Resources/Site/Code/index.php
@@ -1,5 +1,6 @@
 <?php include('header.php'); ?>
 <?php include('leftnav.php'); ?>
+<div id="splash">#M8</div>
 <div id="content">
 <p>Hello World</p>
 </div>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<!-- Powered my M8 -->";
+echo "<!-- Powered by M8 -->";
 include('Resources/Core/core.php');
 include('Resources/Site/settings.php');
 include('Resources/Site/Code/index.php');
