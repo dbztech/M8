2114b8a015bfe7696e5cb2043d540408090c71c6
Brendan Boyle <brendancboyle@me.com>
2012-04-18 14:22:11 -0500

    Added unveil JavaScript

--- Resources/Site/Code/scripts.js
+++ Resources/Site/Code/scripts.js
@@ -3,3 +3,8 @@
 function load() {
 	document.getElementById('splashcontinue').style.display = "block";
 }
+
+function unveil() {
+	document.getElementById('splashcontinue').className = "unveil";
+	document.getElementById('splashtext').className = "unveil";
+}
