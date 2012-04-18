bc2dac4bcaba66cb9db35393330fd5b3a7842b49
Brendan Boyle <brendancboyle@me.com>
2012-04-18 14:18:12 -0500

    Removed datables test script

--- Resources/Site/Code/index.php
+++ Resources/Site/Code/index.php
@@ -1,7 +1,4 @@
 <?php include('header.php'); ?>
-<?php
-print_r($database->returndata('SELECT * FROM `test` WHERE 1'));
-?>
 <?php include('leftnav.php'); ?>
 <div id="splash"><img src="Resources/Site/Files/Continue.png" alt="Continue" id="splashcontinue" /><p id="splashtext">#M8</p></div>
 <div id="content">
