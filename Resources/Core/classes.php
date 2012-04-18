04b4f32b5a5e4c46719d6518934564ac563613e9
Brendan Boyle <brendancboyle@me.com>
2012-04-18 14:17:43 -0500

    Added database query function to the database class

--- Resources/Core/classes.php
+++ Resources/Core/classes.php
@@ -31,6 +31,40 @@ class database
 		echo 'Connected successfully';
 		mysql_close($link);
     }
+
+    public function returndata($query) {
+    	$link = mysql_connect($this->dbhost, $this->dbuser, $this->dbpassword);
+
+		// make foo the current db
+		$db_selected = mysql_select_db($this->database, $link);
+		if (!$db_selected) {
+		    die ('Can\'t use foo : ' . mysql_error());
+		}
+
+		// Perform Query
+		$result = mysql_query($query);
+
+		// Check result
+		// This shows the actual query sent to MySQL, and the error. Useful for debugging.
+		if (!$result) {
+		    $message  = 'Invalid query: ' . mysql_error() . "\n";
+		    $message .= 'Whole query: ' . $query;
+		    return($message);
+		    die($message);
+		}
+
+		// Use result
+		// Attempting to print $result won't allow access to information in the resource
+		// One of the mysql result functions must be used
+		// See also mysql_result(), mysql_fetch_array(), mysql_fetch_row(), etc.
+		while ($row = mysql_fetch_assoc($result)) {
+		    return($row);
+		}
+
+		// Free the resources associated with the result set
+		// This is done automatically at the end of the script
+		mysql_free_result($result);
+    }
 }
 
 
--- Resources/Site/Code/index.php
+++ Resources/Site/Code/index.php
@@ -1,4 +1,7 @@
 <?php include('header.php'); ?>
+<?php
+print_r($database->returndata('SELECT * FROM `test` WHERE 1'));
+?>
 <?php include('leftnav.php'); ?>
 <div id="splash"><img src="Resources/Site/Files/Continue.png" alt="Continue" id="splashcontinue" /><p id="splashtext">#M8</p></div>
 <div id="content">
