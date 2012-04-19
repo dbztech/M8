e04731702ff8fac289c6dd17246572d0c61f08d2
Brendan Boyle <brendancboyle@me.com>
2012-04-18 19:56:49 -0500

    Updated unveil

--- Resources/Site/Code/scripts.js
+++ Resources/Site/Code/scripts.js
@@ -5,10 +5,17 @@ function load() {
 }
 
 function unveil() {
-	document.getElementById('splashcontinue').className = "unveil";
-	document.getElementById('splashtext').className = "unveil";
-	delay('document.getElementById("splashcontinue").style.right = "100%";',950);
-	delay('document.getElementById("splashtext").style.right = "100%";',950);
+	document.getElementById('splashtext').className = 'unveil';
+	delay("document.getElementById('splashcontinue').className = 'unveil'",250);
+	delay("document.getElementById('splashtext').style.right = '100%'",950);
+	delay("document.getElementById('splashcontinue').style.right = '100%'",1200);
+	delay("document.getElementById('splash').style.display = 'none'",1200);
+	delay("showContent()",1200);
+}
+
+function showContent() {
+	document.getElementById('content').style.display = 'block';
+	document.getElementById('leftnav').style.display = 'block';
 }
 
 function delay(command, time) {
