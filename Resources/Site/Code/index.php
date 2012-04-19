de200ef6b1a32e6bff8192a36257b4cce91bcabb
Brendan Boyle <brendancboyle@me.com>
2012-04-18 19:37:03 -0500

    Added unveil

--- Resources/Site/Code/index.php
+++ Resources/Site/Code/index.php
@@ -1,6 +1,6 @@
 <?php include('header.php'); ?>
 <?php include('leftnav.php'); ?>
-<div id="splash"><img src="Resources/Site/Files/Continue.png" alt="Continue" id="splashcontinue" /><p id="splashtext">#M8</p></div>
+<div id="splash"><img src="Resources/Site/Files/Continue.png" alt="Continue" id="splashcontinue" onClick="unveil();" /><p id="splashtext">#M8</p></div>
 <div id="content">
 <p>Hello World</p>
 </div>
--- Resources/Site/Code/scripts.js
+++ Resources/Site/Code/scripts.js
@@ -7,4 +7,10 @@ function load() {
 function unveil() {
 	document.getElementById('splashcontinue').className = "unveil";
 	document.getElementById('splashtext').className = "unveil";
+	delay('document.getElementById("splashcontinue").style.right = "100%";',950);
+	delay('document.getElementById("splashtext").style.right = "100%";',950);
+}
+
+function delay(command, time) {
+	setTimeout(command, time);
 }
