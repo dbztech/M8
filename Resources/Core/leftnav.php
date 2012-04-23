a36b368c4521778bc8ab273b0f9484e9d0aa6427
Brendan Boyle <brendancboyle@me.com>
2012-04-23 13:52:52 -0500

    Fixed CSS for multiple screen sizes

--- Resources/Core/leftnav.php
+++ Resources/Core/leftnav.php
@@ -1,4 +1,4 @@
 <div id="leftnav">
 <p id="logo">#M8</p>
-<div class="hr" style="width: 280px; height: 2px; margin-top: -12px;"></div>
+<div class="hr" style="width: 95%; height: 2px; margin-top: -12px;"></div>
 </div>
