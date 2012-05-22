494514b259536f503d9d2027f411bf885899c344
Brendan Boyle <brendancboyle@me.com>
2012-05-21 19:38:08 -0500

    Make pages follow the admintable styling

--- Resources/Core/edit.php
+++ Resources/Core/edit.php
@@ -1,7 +1,7 @@
 <div id="edit">
 <h1>Page Info:</h1>
 <form>
-<table border="1" style="margin: auto; width: 98%;">
+<table class="admintable">
 <th>Page Name:</th><th>Page Title:</th><th>Page Description:</th><th>Page Location:</th>
 <?php $page->getallpages(); ?>
 </table>
