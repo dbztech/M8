34dd5c098e8ed9b1bc6b176bb729c188ea01465a
Brendan Boyle <brendan@dbztech.com>
2012-06-19 09:34:57 -0500

    Fixed error where the create user hidden values were out of date

--- Resources/Core/users.php
+++ Resources/Core/users.php
@@ -1,8 +1,8 @@
 <div id="users" style="display: none;">
 <h1>User Info:</h1>
 <form action="/Resources/Core/ajax.php" method="get">
-<input type="hidden" name="verify" value="<?php echo $_COOKIE['sessionhash']; ?>" />
-<input type="hidden" name="username" value="<?php echo $_COOKIE['username']; ?>" />
+<input type="hidden" name="verify" value="<?php echo $login->sessionhash; ?>" />
+<input type="hidden" name="username" value="<?php echo $login->username; ?>" />
 <input type="hidden" name="type" value="adduser" />
 <input type="hidden" name="query" value="nonajax" />
 <p>Username:</p>
